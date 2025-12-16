55f83147542e21d3fff17573bf176d974705b97c
KuroAkai <derudj12345@gmail.com>
2025-12-16 19:52:09 +0700

    [DKD] Membuat logic Bank Sampah dan Halaman Bank Sampah

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ app.use(express.json());
 
 app.use("/api/keuntungan", require("./routes/keuntungan.routes"));
 
+app.use("/api/bankSampah", require("./routes/bankSampah.routes"));
+
 app.listen(PORT, () => {
   console.log("Backend running on http://localhost:" + PORT);
 });
