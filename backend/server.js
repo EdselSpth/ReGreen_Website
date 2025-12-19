567b2cb3d5061f69c4ad8cd1cf22e6bc30058595
Rd27p <g.a.raka.darma.p.k@gmail.com>
2025-12-19 08:17:12 +0700

    fix: [GAR] Perbaikan pada server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 require("./scripts/migrateKeuntungan");
 require("./scripts/migratebankSampah");
+require("./scripts/migrateVideo");
+require("./scripts/migrateArtikel");
 
 const express = require("express");
 const cors = require("cors");
@@ -11,8 +13,8 @@ app.use(express.json());
 
 app.use("/api/keuntungan", require("./routes/keuntungan.routes"));
 app.use("/api/bankSampah", require("./routes/bankSampah.routes"));
-app.use("/api/video", require("./routes/video.routes"));
-app.use("/api/artikel", require("./routes/artikel.routes"));
+app.use("/api/video", require("./routes/videoEdukasi.routes"));
+app.use("/api/artikel", require("./routes/artikelEdukasi.routes"));
 
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
