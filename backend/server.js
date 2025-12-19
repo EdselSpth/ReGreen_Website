9eac7cbc42356c9271059299036cf10a307558a0
Rd27p <g.a.raka.darma.p.k@gmail.com>
2025-12-19 08:02:08 +0700

    feat: [GAR] penambahan logic artikel controller

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ app.use(express.json());
 
 app.use("/api/keuntungan", require("./routes/keuntungan.routes"));
 app.use("/api/bankSampah", require("./routes/bankSampah.routes"));
+app.use("/api/video", require("./routes/video.routes"));
+app.use("/api/artikel", require("./routes/artikel.routes"));
 
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
