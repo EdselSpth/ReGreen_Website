896639914e550198bf88e362b7e6d15f9d8cc69f
tifunnyy <funnyrnd92@gmail.com>
2025-12-20 01:11:08 +0700

    feat:[FNY] menambahkan pendaftaran area user dan admin

---
+++ backend/routes/pendaftaranAreaAdmin.routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/areaAdmin.controller");
+
+router.get("/", controller.getAll);
+router.get("/:id", controller.getById);
+router.put("/:id", controller.update);
+router.delete("/:id", controller.delete);
+
+module.exports = router;
